449d7829e0e924a86229b237b9ff3e48b908a3b8
Mzajirow <alexejirow@gmail.com>
2022-03-14 12:39:55 -0700

    lowercase then uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,10 +7,9 @@
  */
 int main(void)
 {
-	char ch;
-	
+	char ch;	
 	for (ch = 'a'; ch <= 'z'; ch++)
-	       putchar(ch);
+ 		putchar(ch);
 	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
 	putchar('\n');
